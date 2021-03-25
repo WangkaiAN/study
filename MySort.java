29059fc793027c4c98d342316b9d732dcfbabcf9
WangkaiAN <78291260+WangkaiAN@users.noreply.github.com>
2021-03-25 06:28:57 -0800

    5中基本排序

--- MySort.java
+++ MySort.java
@@ -1,36 +1,119 @@
-package java42_0321;
+package Sort;
 
 import java.util.Arrays;
 
 public class MySort {
-    public static void insertSort(int[] arr) {
-        // 这个循环就是在控制代码进行 N 次插入过程
-        for (int bound = 1; bound < arr.length; bound++) {
-            // 循环内部要实现插入一次的过程
-            // 需要找到待排序区间的第一个元素, 放在哪里合适, 并且进行搬运赋值
-            // 已排序区间: [0, bound)
-            // 待排序区间: [bound, length)
-            // 此处的 v 就是待排序区间的第一个元素, 也就是要被插入的元素
-            int v = arr[bound];
-            int cur = bound - 1;
-            for (; cur >= 0; cur--) {
-                if (arr[cur] > v) {
-                    // 如果 cur 位置的元素比待插入元素大
-                    // 说明 v 要插入到 arr[cur] 的前面.
-                    // 就需要把 arr[cur] 给往后搬运
-                    arr[cur + 1] = arr[cur];
-                } else {
-                    // 此时就相当于找到了要放置 v 的位置
+    public static void swep(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    //插入排序
+    public static void InsertSort(int[] arr){
+        //无序区间
+        for(int i = 1;i < arr.length;i++){
+            int v = arr[i];
+            int j= i-1;//有序区间
+            for (;j>=0;j--){
+                if(arr[j]>v){
+                    arr[j+1] = arr[j];
+                }else{
                     break;
                 }
+            }//内层循环退出时，相当于找到了，v要插入的位置
+            arr[j+1] = v;
+        }
+    }
+    //希尔排序
+    public  static void shellSort(int[] arr){
+        int gap = arr.length/2;
+        while(gap>=1){
+            SinsertSort(arr,gap);
+            gap = gap/2;
+        }
+    }
+
+    private static void SinsertSort(int[] arr, int gap) {
+        for(int i = gap;i < arr.length;i++){
+            int v = arr[i];
+            int j= i-gap;//有序区间
+            for (;j>=0;j-=gap){
+                if(arr[j]>v){
+                    arr[j+gap] = arr[j];
+                }else{
+                    break;
+                }
+            }//内层循环退出时，相当于找到了，v要插入的位置
+            arr[j+gap] = v;
+        }
+    }
+    //堆排序
+    public static void heapSort(int[] arr){
+        GreatHeap(arr);
+        int leng = arr.length;
+        for (int i =0;i<arr.length;i++){
+            swep(arr,0,leng-1);
+            leng--;
+            shiftDown(arr,leng,0);
+        }
+    }
+
+    private static void GreatHeap(int[] arr) {
+        for(int i = (arr.length-1-1)/2;i>=0;i--){
+            shiftDown(arr,arr.length,i);
+        }
+    }
+
+    private static void shiftDown(int[] arr, int size, int index) {
+        int parent = index;
+        int child = 2*parent+1;
+        while(child<size){
+            if(child+1<size && arr[child+1]>arr[child]){
+                child = child+1;
             }
-            arr[cur + 1] = v;
+            if(arr[parent]<arr[child]){
+                swep(arr,parent,child);
+            }else{
+                break;
+            }
+            parent = child;
+            child = 2*parent+1;
+        }
+    }
+    //选择排序
+    public static void selectSort(int[] arr){
+        //有序区间在前，无序区间在后
+        for (int i= 0;i<arr.length;i++){
+            int max = i;
+            for (int j= i+1;j<arr.length;j++){
+                // 擂台就是i位置的元素.
+                // 取 j 位置的元素和擂台进行比较.
+                if(arr[j]<arr[max]){
+                    max = j;
+                }
+            }
+            swep(arr,max,i);
+        }
+    }
+    //冒泡排序
+    public static void bulleSort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            //待排区间在前面，已排在后面
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    swep(arr,j,j+1);
+                }
+            }
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {9, 5, 2, 7, 3, 6, 8};
-        insertSort(arr);
+        int[] arr ={4,2,1,7,5,8,6,9,3};
+//        InsertSort(arr);
+//        shellSort(arr);
+//        heapSort(arr);
+        selectSort(arr);
+//        bulleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
