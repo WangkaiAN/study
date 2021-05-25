f8055d5f2f4def3788d44f275af2bd84a23fe39c
WangkaiAN <78291260+WangkaiAN@users.noreply.github.com>
2021-05-25 12:26:58 +0800

    day32 斐波那契凤尾数 淘宝网店

--- Main.java
+++ Main.java
@@ -1,81 +1,121 @@
-package day31;
+package day32;
 
 import java.util.Scanner;
 public class Main{
+    private static int[] leapYds = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+    private static int[] nleapYds = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    private static int[] p={0,2,1,1,2,1,2,1,2,2,2,1,2};
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
-            int n = sc.nextInt();
-            System.out.print(n+" = ");
-            for(int i=2;i<=n/2;i++){//6=2*3
-                while(n%i==0 && n!=i){
-                    System.out.print(i+" * ");
-                    n/=i;
-                }
+            int year1 = sc.nextInt();
+            int month1 = sc.nextInt();
+            int day1 = sc.nextInt();
+            int year2 = sc.nextInt();
+            int month2 = sc.nextInt();
+            int day2 = sc.nextInt();
+
+            int sum=0;
+            if(year1 == year2){
+                sum = commonYear(year1,month1,day1,year2,month2,day2);
+            }else{
+                sum = NotCommonYear(year1,month1,day1,year2,month2,day2);
             }
-            System.out.println(n);
+            System.out.println(sum);
         }
         sc.close();
     }
-//    public static void main(String[] args){
-//        Scanner sc = new Scanner(System.in);
-//        while(sc.hasNext()){
-//            int y = sc.nextInt();
-//            NewYear(y);
-//            Martin(y);
-//            President(y);
-//            Memorident(y);
-//            Independence(y);
-//            Labor(y);
-//            Thanks(y);
-//            Christmas(y);
-//            System.out.println();
+    public static boolean isLeapYear(int year){
+        return year % 400==0 || (year %4==0 && year %100 !=0);
+    }
+    public static int commonYear(int year1,int month1,int day1,int year2,int month2,int day2){
+        int sum=0;
+        if(month1 == month2){
+            for(int i = day1;i<=day2;i++){
+                sum += p[month1];
+            }return sum;
+        }
+        if(isLeapYear(year1)){
+            for(int i=day1;i<= leapYds[month1];i++){
+                sum += p[month1];
+            }
+        }else{
+            for(int i=day1;i<=nleapYds[month1];i++){
+                sum += p[month1];
+            }
+        }
+        month1++;
+        while(month1 < month2){
+            if(isLeapYear(year1)){
+                sum += leapYds[month1] * p[month1];
+            }else{
+                sum += nleapYds[month1] * p[month1];
+            }
+        }
+        for(int i=1;i<=day2;i++){
+            sum +=p[month2];
+        }
+        return sum;
+    }
+    public static int NotCommonYear(int year1,int month1,int day1,int year2,int month2,int day2){
+        int sum = 0;
+        if(isLeapYear(year1)){
+            for(int i=day1;i<=leapYds[month1];i++){
+                sum += p[month1];
+            }
+        }else{
+            for(int i=day1;i<=nleapYds[month1];i++){
+                sum += p[month1];
+            }
+        }
+        month1++;
+        for(;month1 <13;month1++){
+            if(isLeapYear(year1)){
+                sum += leapYds[month1] * p[month1];
+            }else{
+                sum += nleapYds[month1] * p[month1];
+            }
+        }
+        int year = year1+1;
+        while(year<year2){
+            int month = 1;
+            while(month <13){
+                if(isLeapYear(year1)){
+                    sum += leapYds[month] * p[month];
+                }else{
+                    sum += nleapYds[month] * p[month];
+                }
+                month++;
+            }
+            year++;
+        }
+        if(isLeapYear(year2)){
+            for(int i=1 ;i<= month2 ;i++){
+                sum +=leapYds[i] * p[i];
+            }
+        }else{
+            for(int i=1 ;i<= month2 ;i++){
+                sum += nleapYds[i] * p[i];
+            }
+        }
+        for(int i =1;i<=day2;i++){
+            sum +=p[month2];
+        }
+        return sum;
+    }
+    //斐波那切凤尾数
+//        public static void main(String[] args){
+//            Scanner sc = new Scanner(System.in);
+//            int[] result = new int[100001];
+//            result[0] = 1;
+//            result[1] = 1;
+//            for(int i =2;i<100001;i++){
+//                result[i] = (result[i-1] + result[i-2])%1000000;
+//            }
+//            while(sc.hasNext()){
+//                int n = sc.nextInt();
+//                System.out.printf(n<25 ? "%d\n":"%06d\n",result[n]);
+//            }
+//            sc.close();
 //        }
-//        sc.close();
-//    }
-    public static int dayOfWeek(int year,int month,int day){
-        if(month ==1 || month==2){
-            month+=12;
-            year-=1;
-        }
-        int cent = year/100;
-        year %= 100;
-        int week = year+(year/4)+(cent/4)-2*cent +26*(month+1)/10+day-1;
-        week = (week%7 + 7)%7;
-        if(week == 0){
-            week =7;
-        }
-        return week;
-    }
-    public static int dayOfDemand(int year,int month,int count,int d){
-        int week = dayOfWeek(year,month,1);
-        int day = 1 + (count-1) * 7+(7+d-week)%7;
-        return day;
-    }
-    public static void NewYear(int year){
-        System.out.println(year+"-01-01");
-    }
-    public static void Martin(int year){
-        System.out.printf(year+"-01-%02d\n",dayOfDemand(year,1,3,1));
-    }
-    public static void President(int year){
-        System.out.printf(year+"-02-%02d\n",dayOfDemand(year,2,3,1));
-    }
-    public static void Memorident(int year){
-        int week = dayOfWeek(year,6,1);
-        int day = 31 -((week == 1) ? 6:(week-2));
-        System.out.println(year+"-05-"+day);
-    }
-    public static void Independence(int year){
-        System.out.println(year+"-07-04");
-    }
-    public static void Labor(int year){
-        System.out.printf(year+"-09-%02d\n",dayOfDemand(year,9,1,1));
-    }
-    public static void Thanks(int year){
-        System.out.printf(year+"-11-%02d\n",dayOfDemand(year,11,4,4));
-    }
-    public static void Christmas(int year){
-        System.out.println(year+"-12-25");
-    }
 }
