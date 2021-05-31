a7d594cfccb7483e077997a3613137bb1a2185e3
WangkaiAN <78291260+WangkaiAN@users.noreply.github.com>
2021-05-31 22:54:14 +0800

    day37

--- Main.java
+++ Main.java
@@ -1,44 +1,75 @@
-package day35;
+package day37;
 
 import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.Arrays;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        TreeSet<String> set = null;
         while(sc.hasNext()){
-            set = new TreeSet<>();
-            String s1 = sc.nextLine();
-            String s2 = sc.nextLine();
-            int flag=0;
-            for(int i =0;i<s1.length();){
-                StringBuffer s3 = new StringBuffer();
-                if(s1.charAt(i)=='"'){
-                    i++;
-                    flag=1;
-                }
-                if(flag==0){
-                    while(i<s1.length() && s1.charAt(i)!=','){
-                        s3.append(s1.charAt(i));
-                        i++;
-                    }
-                }else{
-                    while(i<s1.length() && s1.charAt(i)!='"'){
-                        s3.append(s1.charAt(i));
-                        i++;
-                    }
-                    flag=0;
-                    i++;
-                }
-                i++;
-                set.add(String.valueOf(s3));
+            int n = Integer.parseInt(sc.nextLine());
+            String[] as = new String[n];
+            boolean[] ab = new boolean[n];
+            for(int i =0;i<n;i++){
+                as[i] = sc.nextLine();
             }
-            if(set.contains(s2)){
-                System.out.println("Ignore");
-            }else{
-                System.out.println("Important!");
+            Arrays.sort(as);
+            for(int i = 0;i<n-1;i++){
+                if(as[i] == as[i+1]){
+                    ab[i] = true;
+                }else if(as[i].length() < as[i+1].length() &&
+                        as[i].equals(as[i+1].substring(0,as[i].length()))&&
+                        as[i+1].charAt(as[i].length())=='/'){
+                    ab[i] = true;
+                }
             }
+            for(int i =0;i<n;i++){
+                if(!ab[i]){
+                    System.out.println("mkdir -p "+as[i]);
+                }
+            }
+            System.out.println();
         }
         sc.close();
     }
 }
+
+//数据库连接池
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class Main{
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int[] arr = null;
+//        while(sc.hasNext()){
+//            int n = Integer.parseInt(sc.nextLine());
+//            arr = new int[n];
+//            for(int i =0;i<n;i++){
+//                String[] as= sc.nextLine().split(" ");
+//                if("connect".equals(as[1])){
+//                    arr[i] = 1;
+//                }else{
+//                    arr[i] = -1;
+//                }
+//            }
+//            //System.out.println(Arrays.toString(arr));
+//            System.out.println(help(arr));;
+//        }
+//        sc.close();
+//    }
+//
+//    private static int help(int[] arr) {
+//        int sum =0;
+//        int max = arr[0];
+//        for(int i =0;i<arr.length;i++){
+//            sum += arr[i];
+//            if(sum>max){
+//                max = sum;
+//            }
+//            if(sum<0){
+//                sum = 0;
+//            }
+//        }
+//        return max;
+//    }
+//}
