4a0b83ab27d72cd03de104e16bd4a6ef804d4bc9
WangkaiAN <78291260+WangkaiAN@users.noreply.github.com>
2021-06-01 22:43:37 +0800

    day38

--- Main.java
+++ Main.java
@@ -1,75 +1,85 @@
-package day37;
-
+package day38;
+//蘑菇阵
 import java.util.Scanner;
-import java.util.Arrays;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
-            int n = Integer.parseInt(sc.nextLine());
-            String[] as = new String[n];
-            boolean[] ab = new boolean[n];
-            for(int i =0;i<n;i++){
-                as[i] = sc.nextLine();
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int num = sc.nextInt();
+            double[][] dp = new double[n+1][m+1];
+            boolean[][] flag = new boolean[n+1][m+1];
+            for(int k=0;k<num;k++){
+                int i = sc.nextInt();
+                int j = sc.nextInt();
+                flag[i][j] = true;
             }
-            Arrays.sort(as);
-            for(int i = 0;i<n-1;i++){
-                if(as[i] == as[i+1]){
-                    ab[i] = true;
-                }else if(as[i].length() < as[i+1].length() &&
-                        as[i].equals(as[i+1].substring(0,as[i].length()))&&
-                        as[i+1].charAt(as[i].length())=='/'){
-                    ab[i] = true;
+            dp[1][1] = 1.0;
+            for(int i =1;i<=n;i++){
+                for(int j=1;j<=m;j++){
+                    if(!(i==1&&j==1)){
+                        if(flag[i][j] == true){
+                            dp[i][j] = 0;
+                        }else{
+                            dp[i][j] = dp[i-1][j]*(j==m? 1: 0.5)+dp[i][j-1]*(i==n? 1: 0.5);
+                        }
+                    }
                 }
             }
-            for(int i =0;i<n;i++){
-                if(!ab[i]){
-                    System.out.println("mkdir -p "+as[i]);
-                }
-            }
-            System.out.println();
+            System.out.println(String.format("%.2f",dp[n][m]));
         }
         sc.close();
     }
 }
 
-//数据库连接池
-//import java.util.Arrays;
+//红与黑，广度优先遍历，bps
 //import java.util.Scanner;
-//
 //public class Main{
+//    static char[][] g = new char[20][20];
+//    static int[] dx = {-1,0,1,0};
+//    static int[] dy = {0,1,0,-1};
+//    static int n,m,x,y;
 //    public static void main(String[] args){
 //        Scanner sc = new Scanner(System.in);
-//        int[] arr = null;
 //        while(sc.hasNext()){
-//            int n = Integer.parseInt(sc.nextLine());
-//            arr = new int[n];
-//            for(int i =0;i<n;i++){
-//                String[] as= sc.nextLine().split(" ");
-//                if("connect".equals(as[1])){
-//                    arr[i] = 1;
-//                }else{
-//                    arr[i] = -1;
+//            m = sc.nextInt();
+//            n = sc.nextInt();
+//            if(n == 0 && m == 0){
+//                return;
+//            }
+//            for(int i=0;i<m;i++){
+//                String s = sc.next();
+//                g[i] = s.toCharArray();
+//                for(int j =0;j<n;j++){
+//                    if(g[i][j] == '@'){
+//                        x = i;
+//                        y = j;
+//                        g[i][j] = '1';
+//                    }
 //                }
 //            }
-//            //System.out.println(Arrays.toString(arr));
-//            System.out.println(help(arr));;
+//            dfs(x,y);
+//            int ans = 0;
+//            for(int i = 0;i<m;i++){
+//                for(int j =0;j<n;j++){
+//                    if(g[i][j] == '1'){
+//                        ans++;
+//                    }
+//                }
+//            }
+//            System.out.println(ans);
 //        }
 //        sc.close();
 //    }
-//
-//    private static int help(int[] arr) {
-//        int sum =0;
-//        int max = arr[0];
-//        for(int i =0;i<arr.length;i++){
-//            sum += arr[i];
-//            if(sum>max){
-//                max = sum;
-//            }
-//            if(sum<0){
-//                sum = 0;
+//    public static void dfs(int x,int y){
+//        g[x][y] = '1';
+//        for(int i =0;i<4;i++){
+//            int a = x+dx[i];
+//            int b = y+dy[i];
+//            if(a>=0 && a<m &&b>=0 && b<n && g[a][b] == '.'){
+//                dfs(a,b);
 //            }
 //        }
-//        return max;
 //    }
 //}
