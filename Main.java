75b92fc1feb17413711e047b99a490434aae6aa7
WangkaiAN <78291260+WangkaiAN@users.noreply.github.com>
2021-06-03 17:43:10 +0800

    day40

--- Main.java
+++ Main.java
@@ -1,55 +1,58 @@
-package day39;
+package day40;
 
-//最长公共子序列（动态规划）
+import java.util.Scanner;
+
+//最长上升子序列
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int n = sc.nextInt();
+            int[] ar = new int[n];
+            for(int i=0;i<n;i++){
+                ar[i] = sc.nextInt();
+            }
+            System.out.println(help(ar));
+        }
+        sc.close();
+    }
+    public static int help(int[] arr){
+        int[] dp = new int[arr.length];
+        dp[0] = 1;
+        int ans = 1;
+        for(int i =1;i<arr.length;i++){
+            dp[i] = 1;
+            for(int j = 0;j<i;j++){
+                if(arr[j] < arr[i]){
+                    dp[i] = Math.max(dp[j]+1,dp[i]);
+                }
+            }
+            ans = Math.max(dp[i],ans);
+        }
+        return ans;
+    }
+}
+//发邮件
 //import java.util.Scanner;
 //public class Main{
-//    public static void main(String[] args){
+//    public static v7oid main(String[] args){
 //        Scanner sc = new Scanner(System.in);
 //        while(sc.hasNext()){
-//            String s1 = sc.next();
-//            String s2 = sc.next();
-//            //System.out.println(s1+" "+s2);
-//            System.out.println(find(s1,s1.length(),s2,s2.length()));
+//            int n = sc.nextInt();
+//            System.out.println(help(n));
 //        }
 //        sc.close();
 //    }
-//    public static int find(String s1,int n,String s2,int m){
-//        int[][] dp = new int[n+1][m+1];
-//        for(int i =1;i<=n;i++){
-//            for(int j = 1;j<=m;j++){
-//                if(s1.charAt(i-1) == s2.charAt(j-1)){
-//                    dp[i][j] = dp[i-1][j-1]+1;
-//                }else{
-//                    dp[i][j] = dp[i-1][j] > dp[i][j-1] ? dp[i-1][j]:dp[i][j-1];
-//                }
-//            }
+//    public static long help(int n){
+//        long[] dp = new long[n+1];
+//        dp[1] = 0;
+//        dp[2] = 1;
+//        if(n == 2){
+//            return dp[2];
 //        }
-//        return dp[n][m];
-//    }
-//}
-
-//字符串计数
-//import java.util.Scanner;
-//public class Main{
-//    public static void main(String[] args){
-//        Scanner sc = new Scanner(System.in);
-//        while(sc.hasNext()){
-//            String s1 = sc.next();
-//            String s2 = sc.next();
-//            int len1 = sc.nextInt();
-//            int len2 = sc.nextInt();
-//            char[] ch1 = s1.toCharArray();
-//            char[] ch2 = s2.toCharArray();
-//            int res = 0;
-//            int[] dp = new int[len2+1];
-//            for(int i = 1;i<= len2;i++){
-//                dp[i] = (dp[i-1]*26)%1000007;
-//                if(i<=s1.length()) dp[i] = dp[i]-(ch1[i-1]-96);
-//                if(i<=s2.length()) dp[i] = dp[i]+(ch2[i-1]-96);
-//                if(i>=len1) res+=dp[i];
-//            }
-//            System.out.println((res-1)%1000007);
+//        for(int i=3;i<=n;i++){
+//            dp[i] = (i-1)*(dp[i-1]+dp[i-2]);
 //        }
-//        sc.close();
+//        return dp[n];
 //    }
 //}
