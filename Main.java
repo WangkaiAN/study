94faa2f9d5f775544cad82cb580ccbffe1a064c8
WangkaiAN <78291260+WangkaiAN@users.noreply.github.com>
2021-06-09 09:22:33 +0800

    day44

--- Main.java
+++ Main.java
@@ -1,78 +1,59 @@
-package day43;
-//求和
+package day44;
+
+/*
+/单词倒排
+ */
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Main{
-    private static ArrayList<ArrayList<Integer>> res = new ArrayList<>();
-    private static ArrayList<Integer> list = new ArrayList<>();
-
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = null;
         while(sc.hasNext()){
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            dfs(1,0,m,n);
-            for(ArrayList<Integer> l :res){
-                for(int i =0;i<l.size();i++){
-                    System.out.print(l.get(i));
-                    if(i != l.size()-1){
-                        System.out.print(" ");
+            String s = sc.nextLine();
+            list = new ArrayList<>();
+            int i = 0;
+            while(i<s.length()){
+                if((s.charAt(i)<='z'&& s.charAt(i)>='a')||(s.charAt(i)<='Z'&& s.charAt(i)>='A')){
+                    int j=i+1;
+                    while(j<s.length() && ((s.charAt(j)<='z'&& s.charAt(j)>='a')||(s.charAt(j)<='Z'&& s.charAt(j)>='A'))){
+                        j++;
                     }
+                    list.add(s.substring(i,j));
+                    i=j;
+                }else{
+                    i++;
                 }
-                System.out.println();
+            }
+            for(int k = list.size()-1;k>=0;k--){
+                System.out.print(list.get(k));
+                if(k!=0){
+                    System.out.print(" ");
+                }
             }
         }
         sc.close();
     }
-    public static void dfs(int index,int sum,int target,int n){
-        if(sum>=target){
-            if(sum==target){
-                res.add(new ArrayList<>(list));
-            }
-            return;
-        }
-
-        for(int i = index;i<=n;i++){
-            if(i>target) {
-                continue;
-            }
-            list.add(i);
-            dfs(i+1,sum+i,target,n);
-            list.remove(list.size()-1);
-        }
-    }
 }
 
-//电话号码
+/*
+* 驼峰命名法
+* */
 //import java.util.Scanner;
-//import java.util.Collections;
-//import java.util.ArrayList;
 //public class Main{
 //    public static void main(String[] args){
-//        String sy = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-//        String nu = "222333444555666777788899991234567890";
-//        ArrayList<String> list = null;
 //        Scanner sc = new Scanner(System.in);
 //        while(sc.hasNext()){
-//            list = new ArrayList<>();
-//            int n = Integer.parseInt(sc.nextLine());
-//            for(int i = 0;i<n;i++){
-//                String str = sc.nextLine();
-//                str = str.replace("-","");
-//                String result = "";
-//                for(int j=0;j<7;j++){
-//                    result += nu.charAt(sy.indexOf(str.charAt(j)+""));
-//                }
-//                result = result.substring(0,3)+"-"+result.substring(3,7);
-//                if(!list.contains(result)){
-//                    list.add(result);
+//            String s = sc.nextLine();
+//            char[] ac = s.toCharArray();
+//            for(int i=0,j=1;j<s.length();i++,j++){
+//                if(ac[i]=='_'){
+//                    ac[j]=(char)(ac[j]-32);
 //                }
 //            }
-//            Collections.sort(list);
-//            for(int j = 0;j<list.size();j++){
-//                System.out.println(list.get(j));
-//            }
-//            System.out.println();
+//            String s1 = new String(ac);
+//            s1 = s1.replaceAll("_","");
+//            System.out.println(s1);
 //        }
 //        sc.close();
 //    }
