bb7b432df0589d9ad596c82289519f267eac2661
WangkaiAN <78291260+WangkaiAN@users.noreply.github.com>
2021-06-18 23:22:27 +0800

    day45

--- Main.java
+++ Main.java
@@ -1,58 +1,91 @@
-package day46;
+package day45;
 
-//错误简单的记录
-import java.util.*;
+//查找兄弟单词
+//题目的坑在于没有说出输入格式，自测不通过的格式，提交时可以通过的
+import java.util.Scanner;
+import java.util.Arrays;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        Map<String,Integer> map = new LinkedHashMap<>();
         while(sc.hasNext()){
-            String str = sc.next();
-            int line = sc.nextInt();
-            String[] arr = str.split("\\\\");
-            String s = arr[arr.length-1];
-            if(s.length()>16){
-                s = s.substring(s.length()-16);
+            int n = sc.nextInt();
+            String[] str = new String[n];//存放输入的字符串
+            String[] result = new String[n+1];//存放字典排序后的兄弟字符串，
+            // 这里假设输入的字符串都是目标单词的兄弟单词，看题目可以知道，返回的不是下标，而是+1
+            for(int i = 0;i<n;i++){
+                str[i] = sc.next();
             }
-            String key = s + " "+line;
-            if(!map.containsKey(key)){
-                map.put(key,1);
-            }else{
-                map.put(key,map.get(key)+1);
+            Arrays.sort(str);
+            String s = sc.next();
+            int x = sc.nextInt();
+            int count = 0,k=1;
+            boolean flag = false;
+            for(int i = 0;i<n;i++){//依次遍历字符串数组，核目标字符串作比较
+                if(!s.equals(str[i])){
+                    flag = help(s,str[i]);
+                    if(flag){
+                        count++;
+                        result[k++] = str[i];
+                    }
+                }
+            }
+            System.out.println(count);
+            if(count >= x){
+                System.out.println(result[x]);
             }
         }
-        int count = 0;
-        for(String string : map.keySet()){
-            count++;
-            if(count>(map.keySet().size()-8)){
-                System.out.println(string + " " + map.get(string));
-            }
+        sc.close();
+    }
+    public static boolean help(String s1, String s2){
+        if(s1.length() != s2.length()){
+            return false;
         }
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        return new String(c1).equals(new String(c2));
     }
 }
-
-//数据中出现次数超过一半的数字
-
-//import java.util.Arrays;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        int[] ar = new int[]{1,2,3,2,2,2,5,4,2};
-//        System.out.println(MoreThanHalfNum_Solution(ar));
-//    }
-//    public static int MoreThanHalfNum_Solution(int [] array) {
-//        int n = array.length/2;
-//        Arrays.sort(array);
-//        int x = array[n];
-//        int count=1;
-//        for(int i =0;i<array.length;i++){
-//            if(array[i] == x){
-//                count++;
+//乒乓球筐
+//import java.util.Scanner;
+//import java.util.HashMap;
+//public class Main{
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        HashMap<Character,Integer> map1 = null;
+//        HashMap<Character,Integer> map2 = null;
+//        while(sc.hasNext()){
+//            String s1 = sc.next();
+//            String s2 = sc.next();
+//            map1 = new HashMap<>();
+//            map2 = new HashMap<>();
+//            map1 = help(s1);
+//            map2 = help(s2);
+//            //System.out.println(map1);
+//            //System.out.println(map2);
+//            int flag=0;
+//            for(Character key : map2.keySet()){
+//                if(map2.get(key)>map1.getOrDefault(key,0)){
+//                    System.out.println("No");
+//                    flag = 1;
+//                    break;
+//                }
+//            }
+//            if(flag==0){
+//                System.out.println("Yes");
 //            }
 //        }
-//        if(count>n){
-//            return x;
+//        sc.close();
+//    }
+//    public static HashMap<Character,Integer> help(String s){
+//        HashMap<Character,Integer> map = new HashMap<>();
+//        for(int i =0;i<s.length();i++){
+//            char c = s.charAt(i);
+//            int value = map.getOrDefault(c,0);
+//            map.put(c,value+1);
 //        }
-//        return 0;
+//        return map;
 //    }
 //}
