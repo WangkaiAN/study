f4d1e693b3b5fe34fe7695ab47d425d28a2ef93c
WangkaiAN <78291260+WangkaiAN@users.noreply.github.com>
2021-06-24 10:56:36 +0800

    day49

--- Main.java
+++ Main.java
@@ -1,39 +1,54 @@
-package day48;
+package day49;
 
 import java.util.ArrayList;
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
 
-class TreeLinkNode {
-    int val;
-    TreeLinkNode left = null;
-    TreeLinkNode right = null;
-    TreeLinkNode next = null;
-
-    TreeLinkNode(int val) {
+    public TreeNode(int val) {
         this.val = val;
+
     }
+
 }
 public class Main {
     public static void main(String[] args) {
 
     }
-    public static TreeLinkNode GetNext(TreeLinkNode pNode) {
-                if(pNode == null) return null;
-                if(pNode.right != null){//有右子树，则找到右子树的最左侧结点
-                    pNode = pNode.right;
-                    while(pNode.left != null){
-                        pNode = pNode.left;
-                    }
-                    return pNode;
-                }
-                while(pNode.next != null){//没有右子树，找到当前节点满足自己是父结点的左孩子的结点
-                    if(pNode.next.left == pNode){
-                        return pNode.next;
-                    }
-                    pNode = pNode.next;
-                }
-                //遇到了根节点还是没有找到，
-                return null;
-
+    public static ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+    public ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
+        result = new ArrayList<>();
+        if(pRoot == null){
+            return result;
         }
-
+        helper(pRoot,0);
+        for(int i = 0;i<result.size();i++){
+            if(i%2 != 0){
+                help(result.get(i));
+            }
+        }
+        return result;
+    }
+    public void helper(TreeNode root,int level){
+        if(level == result.size()){
+            result.add(new ArrayList<>());
+        }
+        result.get(level).add(root.val);
+        if(root.left != null){
+            helper(root.left,level+1);
+        }
+        if(root.right != null){
+            helper(root.right,level+1);
+        }
+    }
+    public void help(ArrayList list){
+        int[] arr = new int[list.size()];
+        for(int i =0;i<list.size();i++){
+            arr[i] = (int)list.get(i);
+        }
+        for(int i = 0;i<list.size();i++){
+            list.set(i,arr[arr.length-1-i]);
+        }
+    }
 }
