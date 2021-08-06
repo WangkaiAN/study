3f4c8dad0c5263a46e862ccc1dbc9f8ae2378d4e
WangkaiAN <78291260+WangkaiAN@users.noreply.github.com>
2021-08-07 00:06:10 +0800

    day61

--- Main.java
+++ Main.java
@@ -1,19 +1,12 @@
-package day52;
+package day61;
+
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(help(10));
-    }
-    public static int help(int n){
-        if(n==0){
-            return 0;
-        }
-        int num=0;
-        while(n!=0){
-            num+=n%10;
-            n=n/10;
-        }
-        return num;
+        char c = 'a';
+        c = 'a';
+//        c = "a";
+//        Queue<Integer> queue = new Queue<>();
     }
 }
-
